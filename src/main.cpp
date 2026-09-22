1135b2d75fa9ef01ea6979ef8104bd8f012f2a09
Joel A. Jaffe <joel@jaffesd.com>
2026-09-21 22:44:55 -0700

    Actually make it do thruput lol (96kHz, blocksize 4)

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,20 @@ using namespace daisy;
 
 static DaisySeed hardware;
 
+// copy each input channel straight to its output
+static void AudioCallback(AudioHandle::InputBuffer in,
+                          AudioHandle::OutputBuffer out,
+                          size_t size) {
+  for (size_t i = 0; i < size; i++) {
+    out[0][i] = in[0][i];
+    out[1][i] = in[1][i];
+  }
+}
+
 int main() {
   hardware.Init();
 
-  hardware.StartLog(); 
+  hardware.StartLog();
   // hardware.StartLog(true); // waits for connection before proceeding
 
   System::Delay(200);
@@ -16,12 +26,19 @@ int main() {
   hardware.PrintLine("===========================================");
   hardware.PrintLine("");
 
+  // audio passthru
+  hardware.SetAudioBlockSize(4);
+  hardware.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_96KHZ);
+  hardware.StartAudio(AudioCallback);
+  hardware.PrintLine("Audio passthru running @ %d Hz, block size %d",
+                     (int)hardware.AudioSampleRate(),
+                     (int)hardware.AudioBlockSize());
+
   // blink
   while (true) {
     static bool ledState = false;
     ledState = !ledState;
     hardware.SetLed(ledState);
-    hardware.PrintLine("LED is now %s", ledState ? "ON" : "OFF");
     System::Delay(500);
   }
 
