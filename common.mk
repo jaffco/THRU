9ec7814adc332f33736104bf36958ddab958e937
Joel A. Jaffe <joel@jaffesd.com>
2026-09-21 22:45:14 -0700

    Make it a flash app, not SRAM app

--- common.mk
+++ common.mk
@@ -17,9 +17,6 @@ SYSTEM_FILES_DIR := $(LIBDAISY_DIR)/core
 # Set Boot Bin to 10ms version (default is 2000ms)
 BOOT_BIN = $(SYSTEM_FILES_DIR)/dsy_bootloader_v6_3-intdfu-10ms.bin
 
-# Set app type
-APP_TYPE = BOOT_SRAM
-
 # Set the C++ standard
 CPP_STANDARD = -std=gnu++14
 
